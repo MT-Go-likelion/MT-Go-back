891192986aa58d2de5bf260faa6364011b514dc5
Dohun Kim <dohun8758@naver.com>
2023-08-06 17:18:30 +0900

    feature/shopping: modify

--- MT_Goo/shopping/serializers.py
+++ MT_Goo/shopping/serializers.py
@@ -9,4 +9,13 @@ class shoppingMainSerializer(serializers.ModelSerializer):
 class createShoppingSerializer(serializers.ModelSerializer):
     class Meta:
         model = shoppingMain
+<<<<<<< Updated upstream
         fields = ['user', 'item', 'price', 'amount']
+=======
+        fields = ['item', 'price', 'amount']
+        
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['user'] = user
+        return super().create(validated_data)
+>>>>>>> Stashed changes
--- MT_Goo/shopping/views.py
+++ MT_Goo/shopping/views.py
@@ -11,11 +11,16 @@ class shoppingMainView(APIView):
         shopping = shoppingMain.objects.all()
         serializer = shoppingMainSerializer(shopping, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
 class createShoppingView(APIView):
     permission_classes = [IsAuthenticated]
+
     def post(self, request, format=None):
+<<<<<<< Updated upstream
         serializer = createShoppingSerializer(data=request.data)
+=======
+        serializer = createShoppingSerializer(data=request.data, context={'request': request})
+>>>>>>> Stashed changes
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
