e8b266f533ecdd05684b0ef59e6835db5ab88a04
Dohun Kim <dohun8758@naver.com>
2023-08-07 14:48:03 +0900

    feature/lodging: add review api

--- MT_Goo/lodging/urls.py
+++ MT_Goo/lodging/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import lodgingMainView, createLodgingView, lodgingDetailView, createReviewView, lodgingScrapView
+from .views import lodgingMainView, createLodgingView, lodgingDetailView, createReviewView, lodgingScrapView, lodgingReviewView
 
 urlpatterns = [
     path('main/', lodgingMainView.as_view(), name='lodgingMain'),
     path('create/', createLodgingView.as_view(), name='createLodging'),
     path('detail/<int:pk>/', lodgingDetailView.as_view(), name='lodgingDetail'),
+    path('detail/<int:pk>/review/', lodgingReviewView.as_view(), name='lodgingReviewView'),
     path('createReview/', createReviewView.as_view(), name='createReview'),
     path('scrap/', lodgingScrapView.as_view(), name='createScrap'),
 ]
