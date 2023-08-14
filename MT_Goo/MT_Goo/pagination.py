f6600bbf041559b6666f2eaa2d3ba10a9551dda3
Dohun Kim <dohun8758@naver.com>
2023-08-15 01:49:42 +0900

    feature/error: modify

--- MT_Goo/MT_Goo/pagination.py
+++ MT_Goo/MT_Goo/pagination.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
 class lodingListPagination(PageNumberPagination):
-    page_size = 4  
+    page_size = 1  
 class recreationListPagination(PageNumberPagination):
     page_size = 4
 class reviewListPagination(PageNumberPagination):
